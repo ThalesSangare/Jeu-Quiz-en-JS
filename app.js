419fe519c05a95a3a4d54d381e4a40fc0027d2ec
Thales7 <sangarekabinet7@gmail.com>
2026-01-08 01:31:54 +0100

    Ajour du btn soumettre

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
 
+  // ============================
+  // DONNÉES : BONNES RÉPONSES
+  // L'ordre correspond aux questions
+  // ============================
+  const bonnesReponses = [
+    "Thales",
+    "Librairie",
+    "Steve Jobs",
+    "Ancien president",
+    "Developpeur"
+  ];
+
+  // ============================
   // Fonction pour créer une question
-  function creerQuestion(titre, reponses) {
+  // ============================
+  function creerQuestion(titre, reponses,indexQuestion) {
     const questionDiv = document.createElement("div");
     questionDiv.classList.add("question");
 
@@ -17,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const checkbox = document.createElement("input");
       checkbox.type = "radio"; // radio = 1 seul choix
-      checkbox.name = titre; // pour regroupe selon le titre
+      checkbox.name = "question" + indexQuestion; // pour regroupe selon le titre
       checkbox.value = reponse;
 
       const label = document.createElement("label");
@@ -33,26 +47,87 @@ document.addEventListener("DOMContentLoaded", () => {
     container.appendChild(questionDiv);
   }
 
+  // fonction gerer les responses
+ 
+
   // ============================
   // QUESTIONS
   // ============================
 
-  creerQuestion("Qui est le futur grand dev ?", ["Thales", "hawa", "myriam"]);
+creerQuestion("Qui est le futur grand dev ?", [
+    "Thales", 
+    "hawa", 
+    "myriam"
+    ],0
+  );
 
-  creerQuestion("React est t-il un ?", ["framework", "Librairie", "Langage"]);
+creerQuestion("React est t-il un ?", [
+    "framework", 
+    "Librairie", 
+    "Langage"],1);
 
-  creerQuestion("Qui est le Fondateur de Apple ?", [
+creerQuestion("Qui est le Fondateur de Apple ?", [
     "Steve Jobs",
     "Barack Obama",
     "Bill Gates",
-  ]);
-  creerQuestion("Qui est Nelson Mandela ?", [
+  ],2);
+creerQuestion("Qui est Nelson Mandela ?", [
     "Ancien president",
     "Acteur",
     "Musicien"
-]);
-  creerQuestion("Qui suis-je ?", [
+],3);
+creerQuestion("Qui suis-je ?", [
     "Developpeur", 
     "ecrivain", 
-    "medecin"]);
+    "medecin"],4);
+
+
+     // ============================
+  // BOUTON SOUMETTRE
+  // ============================
+  const btnSubmit = document.createElement("button");
+  btnSubmit.textContent = "Soumettre";
+  btnSubmit.style.marginTop = "20px";
+
+  container.appendChild(btnSubmit);
+
+
+  // ============================
+  // GESTION DES RÉPONSES
+  // ============================
+  btnSubmit.addEventListener("click", () => {
+    let score = 0;
+
+    // On parcourt chaque bonne réponse
+    bonnesReponses.forEach((bonneReponse, index) => {
+
+      // On récupère la réponse cochée pour cette question
+      const choix = document.querySelector(
+        `input[name="question${index}"]:checked`
+      );
+
+      // Si l'utilisateur a répondu
+      if (choix) {
+        // Comparaison simple
+        if (choix.value === bonneReponse) {
+          score++;
+        }
+      }
+    });
+
+    if(score === 5){
+      alert('Felicitation ! ton score est : ' + score + " / " + bonnesReponses.length);
+    }else{
+          alert("Ton score est : " + score + " / " + bonnesReponses.length);
+
+    }
+  });
+
+
+
+
+
+
+
+
 });
